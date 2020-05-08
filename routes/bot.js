1c40058e761dfac2cf6f490af91415c68148876f
jmjles <jmjles@gmail.com>
2020-05-08 15:43:10 -0700

    Transferred function to seperate file, Optimized Code, Corrected Timestamps

--- routes/bot.js
+++ routes/bot.js
@@ -1,106 +1,67 @@
 require("dotenv").config();
 const route = require("express").Router();
 const { Client, MessageEmbed } = require("discord.js");
-const moment = require("moment");
 const client = new Client();
 const token = process.env.TOKEN;
-const { goldPrice, price } = require("./botFunctions");
-const itemList = require("../assets/items.json");
-let prefix = "n!";
+const { goldPriceLookup, priceLookup } = require("./botFunctions");
+let prefix = "`";
 let lang = "EN-US";
+const latom = "./assets/latom.png";
 client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}`);
 });
 
 client.on("message", async (msg) => {
   const input = msg.content.split(" ");
-  if (input[0] === prefix) {
-    if (input[1].match(/zevax/i)) {
-      msg.channel.send("Tell lunars we'll be waiting");
-    }
-    if (input[1].match(/price/i)) {
-      let name = "";
-      for (let i = 2; i < input.length; i++) {
-        name = name + " " + input[i];
-        name = name.trim();
-      }
-      //!Albion Market
-      //Item Price Lookup
-      console.log(name);
-      let regex = new RegExp(name, "gi");
-      let filtered = itemList.filter((item) => item.LocalizedNames);
-      filtered = filtered.filter(
-        (item) =>
-          !item.LocalizationNameVariable.match(new RegExp("nontradable", "gi"))
-      );
-      filtered = filtered.filter((item) =>
-        item.LocalizedNames[lang].match(regex)
-      );
-      console.log(filtered);
-      const embedmsg = new MessageEmbed()
-        .setTitle("Search Results")
-        .setDescription(
-          filtered.map(
-            (item) =>
-              `${item.UniqueName.substring(0, 2)}${
-                item.UniqueName.includes("@")
-                  ? `.${item.UniqueName.charAt(item.UniqueName.length - 1)}`
-                  : ""
-              } ${item.LocalizedNames[lang]}`
-          )
-        );
-      const data = await price(filtered[0].UniqueName);
-      console.log(data)
-      const results = []
-      data.forEach((item) => results.push(
-        `${item.city}\n Sell Price: ${item.sell_price_min} ${moment(
-          item.sell_price_min_date.split("T")[1],
-          "h:mm"
-        ).fromNow()}\n Buy Price: ${item.buy_price_min} ${moment(
-          item.buy_price_min_date.split("T")[1],
-          "h:mm"
-        ).fromNow()}\n\n`
-      ));
-      console.log(results)
-      const embedprice = new MessageEmbed()
-        .setTitle(
-          `${filtered[0].UniqueName.substring(0, 2)}${
-            filtered[0].UniqueName.includes("@")
-              ? `.${filtered[0].UniqueName.charAt(filtered[0].UniqueName.length - 1)}`
-              : ""
-          } ${filtered[0].LocalizedNames[lang]}`
-        )
-        .setDescription(results);
-      msg.channel.send(embedprice);
-    }
-    //Gold Price
-    if (input[1].match(/gold/i)) {
-      const data = await goldPrice();
-      const prices = [];
-      data.forEach((e) =>
-        prices.push(
-          `silver ${e.price} ${moment(
-            e.timestamp.split("T")[1],
-            "h:mm"
-          ).fromNow()}\n`
-        )
-      );
-      const embedmsg = new MessageEmbed()
-        .setTitle("Recent Gold Prices")
-        .setDescription(prices)
-        .setTimestamp(Date.now());
-      msg.channel.send(embedmsg);
+  const cmd = input[1].toLowerCase();
 
-      //! General
-      //Set Prefix
-    } else if (input[1].match(/prefix/i)) {
-      if (input[2]) {
-        prefix = input[2];
-        msg.channel.send(`Prefix is now set to: ${prefix}`);
-      } else {
-        msg.channel.send(
-          `Current prefix is: ${prefix}\nTo change the prefix, use the following command: \`\`\`${prefix} prefix [NewPrefix]\`\`\``
-        );
+  if (input[0] === prefix) {
+    switch (cmd) {
+      case "zevax":
+        return msg.channel.send("Nothing to worry about now @Lunars");
+
+      //! Albion Market Commands
+
+      //* Price Lookup
+      case "price": {
+        let name = "";
+        for (let i = 2; i < input.length; i++) {
+          name = name + " " + input[i];
+          name = name.trim();
+        }
+
+        const results = await priceLookup(name, lang);
+
+        const embedprice = new MessageEmbed()
+          .setTitle(
+            `${results.uniName.substring(0, 2)}${
+              results.uniName.includes("@")
+                ? `.${results.uniName.charAt(results.uniName.length - 1)}`
+                : ""
+            } ${results.name}`
+          )
+          .setDescription(results.results);
+        return msg.channel.send(embedprice);
+      }
+
+      //* Gold Lookup
+      case "gold": {
+        const results = await goldPriceLookup();
+        const embedmsg = new MessageEmbed()
+        .setTitle("Recent Gold Prices")
+        .setDescription(results)
+        return msg.channel.send(embedmsg);
+      }
+      case 'latom':
+      case 'f':{
+      const embedmsg = new MessageEmbed()
+        .attachFiles([latom])
+        .setImage("attachment://latom.png");
+      return msg.channel.send(embedmsg);
+      }
+
+      default: {
+        msg.channel.send("Help menu coming soon...");
       }
     }
   }
--- routes/botFunctions.js
+++ routes/botFunctions.js
@@ -1,13 +1,45 @@
-const axios = require('axios')
-module.exports.goldPrice = async () => {
-    const {data} = await axios.get(
-      " https://www.albion-online-data.com/api/v2/stats/gold?count=5"
-    );
-    return data
-}
-module.exports.price = async item => {
-    const { data } = await axios.get(
-      `https://www.albion-online-data.com/api/v2/stats/prices/${item}?qualities=1`
-    );
-    return data
-}
+const axios = require("axios");
+const itemList = require("../assets/items.json");
+const moment = require("moment");
+module.exports.goldPriceLookup = async () => {
+  const { data } = await axios.get(
+    " https://www.albion-online-data.com/api/v2/stats/gold?count=5"
+  );
+  return data.map(
+    (e) =>
+      `silver ${e.price} ${moment(e.timestamp).utc(true).fromNow()}\n`
+  );
+};
+
+//* Price Lookup
+module.exports.priceLookup = async (name, lang) => {
+  let filtered = itemList.filter((item) => item.LocalizedNames);
+  let regex = new RegExp("nontradable", "gi");
+
+  filtered = filtered.filter(
+    (item) => !item.LocalizationNameVariable.match(regex)
+  );
+
+  regex = new RegExp(name, "gi");
+  filtered = filtered.filter((item) => item.LocalizedNames[lang].match(regex));
+
+  const { data } = await axios(
+    `https://www.albion-online-data.com/api/v2/stats/prices/${filtered[0].UniqueName}?qualities=1`
+  );
+  const results = [];
+  data.forEach((item) =>
+    item.city.match(".*\\d.*")
+      ? null
+      : results.push(
+          `${item.city}\n Sell Price: ${item.sell_price_min} | ${moment(
+            item.sell_price_min_date).utc(true).fromNow()}\n Buy Price: ${item.buy_price_min} | ${moment(
+            item.buy_price_min_date).utc(true).fromNow()}\n\n`
+        )
+  );
+  console.log(results);
+  return {
+    uniName: filtered[0].UniqueName,
+    name: filtered[0].LocalizedNames[lang],
+    results,
+  };
+};
